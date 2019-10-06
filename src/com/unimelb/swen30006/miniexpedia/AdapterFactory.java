c858ef9f36b0041caaf53ecfb3479f1d7348c785
jacob <hanxu.jacobhan@gmail.com>
2019-10-06 14:25:46 +1100

    change pattern

--- src/com/unimelb/swen30006/miniexpedia/AdapterFactory.java
+++ src/com/unimelb/swen30006/miniexpedia/AdapterFactory.java
@@ -1,11 +1,13 @@
 package com.unimelb.swen30006.miniexpedia;
 
 public class AdapterFactory {
-	private static AdapterFactory adapter = new AdapterFactory();
+	private static AdapterFactory factory;
+	//new AdapterFactory();
 	private IsearchAdapter searchAdapter = null;
 	private AdapterFactory() {}
 	public static AdapterFactory getInstance() {
-		return adapter;
+		if(factory == null) factory = new AdapterFactory();
+		return factory;
 	}
 	public IsearchAdapter getSearchAdapter(String airline) {
 		if (airline.equals("Jetstar")) {
